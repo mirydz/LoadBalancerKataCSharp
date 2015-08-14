a09980a5c631d1d15afb9b9cde080720c972e794
Michal Rydz <mirydz@gmail.com>
2015-08-14 14:18:25 +0200

    zero test: ItCompiles

---
+++ ServerLoadBalancer.Tests/ServerLoadBalancerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ServerLoadBalancer.Tests
+{
+    [TestFixture]
+    class ServerLoadBalancerTest
+    {
+        [Test]
+        public void ItCompiles()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
