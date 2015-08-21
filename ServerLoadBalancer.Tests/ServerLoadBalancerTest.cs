d2c1768634cd6121c2556306096c4223fee87cae
Michal Rydz <mirydz@gmail.com>
2015-08-21 15:38:54 +0200

    test 2 red

--- ServerLoadBalancer.Tests/ServerLoadBalancerTest.cs
+++ ServerLoadBalancer.Tests/ServerLoadBalancerTest.cs
@@ -28,6 +28,18 @@ namespace ServerLoadBalancer.Tests
             Assert.That(theServer, HasLoadPercentageOf(0.0));
         }
 
+        [Test]
+        public void BalancingServerWithOneSlotCapacity_AndOneSlotVm_FillsServerWithVm()
+        {
+            Server theServer = A(Server().WithCapacity(1));
+            Vm theVm = A(Vm().OfSize(1));
+
+            Balance(ListOfServersWith(theServer), ListOfVmsWith(theVm));
+
+            Assert.That(theServer, HasLoadPercentageOf(100.0));
+            Assert.That(theServer.contains(theVm));
+        }
+
         private Constraint HasLoadPercentageOf(double expectedLoadPerentage)
         {
             return new CurrentLoadPercentageConstraint(expectedLoadPerentage);
